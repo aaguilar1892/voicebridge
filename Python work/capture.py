40bcf5970c35f6079c87f66524af61dd33618f0a
Keiran Nelson <keirannnelson@gmail.com>
2025-02-28 02:05:07 -0600

    Added svm class and displayed on window

--- Python work/capture.py
+++ Python work/capture.py
@@ -1,9 +1,21 @@
 # capture.py
 import cv2
 from gesture_recognition import GestureRecognizer
+from svm_classifier import SVM
+
+# Text attributes for window
+org = (50, 100)
+fontFace = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+color = (255, 255, 255)  # White color
+thickness = 2
+
+model_path = 'Python work\\model_training\\alphabet_svm_pipeline.pkl' # path for model weights
 
 def main():
-    cap = cv2.VideoCapture(1)
+    svm = SVM(model_path, 'alphabet')
+
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Cannot open camera")
         return
@@ -21,7 +33,10 @@ def main():
         results = recognizer.process_frame(frame)
         # Draw the landmarks on the frame
         frame = recognizer.draw_landmarks(frame, results)
-
+        # Get prediction from svm
+        pred = svm.predict(results.right_hand_landmarks)
+    
+        cv2.putText(frame, pred, org, fontFace, fontScale, color, thickness)
         cv2.imshow('Camera Feed with Landmarks', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -30,6 +45,7 @@ def main():
     cap.release()
     recognizer.close()
     cv2.destroyAllWindows()
+    print(landmarks)
 
 if __name__ == '__main__':
     main()
