43e9fce776d216c8084b95ec51a853f868a2a57a
jryan4902 <jake.gonzales1569@gmail.com>
2025-04-09 23:35:09 -0500

    Translate page overhaul with new features

--- voicebridge-app/server/create_dataset.py
+++ voicebridge-app/server/create_dataset.py
@@ -14,7 +14,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-DATA_DIR = './python-work/word_recognition/data'
+DATA_DIR = './data'
 
 data = []
 labels = []
