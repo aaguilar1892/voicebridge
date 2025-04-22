941bd60cf55703ab5a2485cd3f90dd2e0870b215
cmmartin14 <corinna.m.martin@gmail.com>
2025-04-22 15:42:40 -0500

    Begin creating basic flashcard structure

--- voicebridge-app/client/src/assets/components/BasicWords.jsx
+++ voicebridge-app/client/src/assets/components/BasicWords.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function Card() {
+
+    return (
+        <div className='flash-card text-8xl font-bold flex justify-center items-center w-175 h-150 p-6 shadow-2xl bg-white rounded-xl border-4 border-gray-500 shadow-yellow-600'>
+            Front side
+        </div>
+    );
+}
+
 const BasicWords = () => {
 
     const handleMouseEnter = (utterance) => {
@@ -12,7 +21,15 @@ const BasicWords = () => {
     };
 
     return (
-        <div className='p-50'>Test</div>
+        <div className='pt-25 pl-10 flex flex-col'>
+            <div>
+                <h1 className='text-3xl font-bold'>Basic Words</h1>
+            </div>
+            <div className='flex flex-col items-center p-5'>
+                <Card></Card>
+            </div>
+            
+        </div>
     );
 };
  
